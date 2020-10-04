34e315cfcb1162e1c5f0661d8c5d3763fa330f3b
jyosh30 <67313262+jyosh30@users.noreply.github.com>
2020-10-04 20:12:54 +0530

    Day2
    Started with String Practice

---
+++ Day2.py
@@ -0,0 +1,50 @@
+# How to Print a value ?
+print("30 days 30 hour challenge")
+print('30 days 30 hour challenge')
+
+# Assigning String to Variable :
+Hours = "thirty"
+print(Hours)
+
+# Indexing using String
+Days = "Thirty Days"
+print(Days[1])
+
+# How to Print the particular character from certain text ?
+Challenge = "I will win"
+print(Challenge[7:10])
+
+# Print the length of character
+Challenge = "I will win"
+print(len(Challenge))
+
+# Convert String into lower character
+Challenge = "I will win"
+print(Challenge.lower())
+
+# String Concatenation and adding space during concatenation
+a = "30 Days"
+b = "30 hours"
+c = a+ " " +b
+print(c)
+
+# casefold() - Usage
+text = "i will win"
+x = text.casefold()
+y = text.capitalize()
+z = text.find("will")
+a = text.isalpha()
+b = text.isalnum()
+print(x)
+print(y)
+print(z)
+print(a)
+print(b)
+
+
+
+
+
+
+
+
