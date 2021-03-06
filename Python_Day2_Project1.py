0ce891ba61533c334116d6513da2ccf908e4eeb0
Jyoshna B N <jyoshna.n30@gmail.com>
2021-03-06 23:25:48 +0530

    Day 2 project 1

---
+++ Python_Day2_Project1.py
@@ -0,0 +1,16 @@
+#Arithmetic operators
+Value1 = int(input ("Enter Value 1 :"))
+Value2 = int(input("Enter Value 2 :"))
+print("Addition")
+print(Value1 + Value2)
+print("Subtarction")
+print(Value1 - Value2)
+print("Multiplication")
+print(Value1 * Value2)
+print("Division")
+print(Value1 / Value2)
+print("Modulus")
+print(Value1 % Value2)
+
+
+
