74647f75116f1438e436ecac222b89b8f123de72
Jyoshna B N <jyoshna.n30@gmail.com>
2021-03-06 23:23:42 +0530

    Day 1

---
+++ Python_Day1.py
@@ -0,0 +1,12 @@
+#Tell me about yorself
+Name = input("Enter your Name :")
+Degree = input("Enter your degree :")
+Specialization = input("Enter your Specialization :")
+Designation = input("Enter your Designation :")
+CompanyName = input("Enter your current company Name :")
+Experience = input("Enter your total exp in years :")
+Field = input("Enter your field(Testing or Development or support) :")
+Domain = input("Enter your current project domain :")
+print(f"Hi, I am {Name}. I have completed my {Degree} ({Specialization}).\
+I am currently working as a {Designation}.I have {Experience} years of Experience in {Field} field.\
+Currently I am working in a {Domain} project.")
